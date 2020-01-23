d45c61561e01bef1daccd4fb7105e482ce34a72f
Nathaniel Starkman <nstarkman@protonmail.com>
2020-01-23 13:42:49 -0500

    update __init__ templates
    Signed-off-by: Nathaniel Starkman <nstarkman@protonmail.com>

--- src/__init__.py
+++ src/__init__.py
@@ -1,33 +1,46 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 # ----------------------------------------------------------------------------
 #
-# TITLE   : src initialization file
+# TITLE   :
+# AUTHOR  :
 # PROJECT :
 #
 # ----------------------------------------------------------------------------
 
-### Docstring and Metadata
-r"""initialization file
+"""initialization file for __________.
+
+Description.
+
+Routine Listings
+----------------
+module
+
 """
 
-# __author__ = ""
+__author__ = ""
 # __copyright__ = "Copyright 2018, "
 # __credits__ = [""]
-# __license__ = "GPL3"
+# __license__ = "MIT"
 # __version__ = "0.0.0"
 # __maintainer__ = ""
 # __email__ = ""
 # __status__ = "Production"
 
-##############################################################################
-### Imports
-
-# General
-
-
-# Project-Specific
+# __all__ = [
+#     ""
+# ]
 
 
 ##############################################################################
+# IMPORTS
+
+# GENERAL
+
+# CUSTOM
+
+# PROJECT-SPECIFIC
+
+
+##############################################################################
+# END
--- src/_sample/__init__.py
+++ src/_sample/__init__.py
@@ -1,28 +1,45 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 # ----------------------------------------------------------------------------
 #
 # TITLE   :
+# AUTHOR  :
+# PROJECT :
 #
 # ----------------------------------------------------------------------------
 
-### Docstring and Metadata
-"""initialization file
+"""initialization file for __________.
+
+description.
+
+Routine Listings
+----------------
+module
+
 """
 
 __author__ = ""
+# __copyright__ = "Copyright 2018, "
+# __credits__ = [""]
+# __license__ = "MIT"
+# __version__ = "0.0.0"
+# __maintainer__ = ""
+# __email__ = ""
+# __status__ = "Production"
+
+# __all__ = [
+#     ""
+# ]
 
 ##############################################################################
-### Imports
+# IMPORTS
 
-## General
+# GENERAL
 
+# CUSTOM
 
-## Project-Specific
-
-
-##############################################################################
+# PROJECT-SPECIFIC
 
 
 ##############################################################################
+# END
